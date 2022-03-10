7b7620e81f9304c7d925c3267c20eb96054f0313
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-03-10 14:30:17 +0100

    add .env / dynamic DB credentials

--- project/project/settings.py
+++ project/project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -70,15 +71,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+env = environ.Env()
+environ.Env.read_env()
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'immotep',
-        'USER': 'root',
-        'PASSWORD': "",
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
         'HOST': "localhost",
         'PORT': "",
         'OPTIONS': {
