285560cd054c173846784733f9f5ce60e9b83f1b
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-03-17 16:20:08 +0100

    fix - resolve conflicts

--- project/adverts/forms.py
+++ project/adverts/forms.py
@@ -5,6 +5,9 @@ from django.views.generic import CreateView
 from .models import Advert
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from adverts.models import Account
 
 
 class CreateAdvertForm(ModelForm):
@@ -49,4 +52,43 @@ class CreateAdvertForm(ModelForm):
             'property_type': forms.Select(choices=property_types),
             'status': forms.Select(choices=statuses),
         }
+        
     
+# Formulaire de création de compte d'après le modèle User de Django
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ( "username", "email", "password1", "password2" )
+        labels = {
+            "username":"Nom d'utilisateur",
+            "email":"Adresse e-mail",
+            "password1":"Mot de passe",
+            "password2":"Confirmer le mot de passe"
+        }
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+
+        if commit:
+            user.save()
+        return user
+
+# Formulaire qui complète le profil d'après le modèle Account
+class AccountForm(forms.ModelForm):
+
+    class Meta:
+        model = Account
+        fields = ( "phone_number", "postal_code" )
+        labels = {
+            "phone_number":"Numéro de téléphone",
+            "postal_code":"Code postal"
+        }
+    
+    def save(self, commit=True):
+        user = super(AccountForm, self).save(commit=False)
+
+        if commit:
+            user.save()
+        return user
--- project/adverts/views.py
+++ project/adverts/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm, AccountForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 from django.urls import reverse
-from django.views import generic
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Advert
 from .forms import CreateAdvertForm
-
-# Create your views here.
 
 
 def home(request):
@@ -46,3 +45,51 @@ def delete_advert(request, id):
         advert.delete()
         return HttpResponseRedirect(reverse('home'))
     return render(request,'delete_advert.html',{'advert': advert})
+
+  
+# REGISTER
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        account_form = AccountForm(request.POST)
+        if form.is_valid() and account_form.is_valid():
+            user = form.save()
+            account = account_form.save(commit=False)
+            account.user = user
+            account.save()
+            account_form.save_m2m()
+
+            login(request, user)           
+            return redirect("adverts:home")
+        else:
+            print(form.errors, account_form.errors)
+            args = {'form': form, 'account_form': account_form}
+            return render(request, 'register.html', args)
+    else:
+        form = NewUserForm()
+        account_form = AccountForm()
+    return render (request, "register.html", {"register_form":form, "account_form":account_form})
+
+# LOGIN
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"Vous êtes connecté en tant que {username}.")
+                return redirect("adverts:home")
+            else:
+                messages.error(request,"Nom d'utilisateur ou mot de passe invalide.")
+        else:
+            messages.error(request,"Nom d'utilisateur ou mot de passe invalide.")
+    form = AuthenticationForm()
+    return render(request, "login.html", {"login_form":form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, f"Vous êtes déconnecté.")
+    return redirect("adverts:home")
