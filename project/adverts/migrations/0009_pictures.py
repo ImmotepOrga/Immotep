e16a180251376aeff1914ace5bf358fd0f4744b0
Romain <axilais.romain@gmail.com>
2022-03-25 09:49:23 +0100

    Fix migrations bugs

--- project/adverts/migrations/0009_pictures.py
+++ project/adverts/migrations/0009_pictures.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('adverts', '0001_initial'),
+        ('adverts', '0008_delete_removed_date'),
     ]
 
     operations = [
